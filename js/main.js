907167b87308ce4154230e3d5f02542ee0c46127
ToshifumiIde <toshifumiide198765@gmail.com>
2020-11-28 23:20:29 +0900

    ゲーム終了の挙動を実装（途中）

--- js/main.js
+++ js/main.js
@@ -5,20 +5,21 @@
   //ゲームスタートの管理//
   ////////////////////////
   let isPlaying = false;//ゲーム実行状況、初期値はfalse
-  let levelRow;//ゲームレベル、最初は空（後でプレーヤーに入力してもらう）
+  let levelRow;         //ゲームレベル、最初は空（後でプレーヤーに入力してもらう）
+  let requestId;        //後述する再描画処理を格納するId
 
   ////////////////
   //canvasの生成//
   ////////////////
   const canvas = document.createElement("canvas");//<canvas></canvas>の生成（生成のみ）
   const ctx = canvas.getContext("2d"); //キャンバス用のメソッドを取得
-  canvas.width = 500; //canvasの幅
-  canvas.height = canvas.width * 0.8; //canvasの高さ
+  canvas.width = 500;                  //canvasの幅
+  canvas.height = canvas.width * 0.8;  //canvasの高さ
   canvas.setAttribute (
     "style",
     "display:block; margin:0 auto; background-color:#eee"
-  ); //canvasのスタイル
-  document.body.appendChild(canvas); //canvasをbodyの直下に配置
+  );                                   //canvasのスタイル
+  document.body.appendChild(canvas);   //canvasをbodyの直下に配置
 
 
   ////////////////////
@@ -45,6 +46,7 @@
         this.dy *= -1;
       }
       if (this.y > canvas.height) {
+        window.cancelAnimationFrame(requestId);
         isPlaying = false;
         gameOver();
       }
@@ -141,29 +143,30 @@
     }
   };
 
-  //////////////////////
-  //ボールの当たり判定//
-  //////////////////////
+  ////////////////////////////////////////
+  //         ボールの当たり判定         //
+  //(obj1にball、obj2にpaddleを後で格納)//
+  ////////////////////////////////////////
   const collide = (obj1, obj2) => {
     return (
-      obj1.x < obj2.x + obj2.width &&
-      obj2.x < obj1.x + obj1.width &&
-      obj1.y < obj2.y + obj2.height &&
-      obj2.y < obj1.y + obj1.height
-    );
+      obj1.x < obj2.x + obj2.width && //ballのx座標がpaddleのx座標+幅より小さい(左側)
+      obj2.x < obj1.x + obj1.width && //ballの幅+x座標がpaddleのx座標(左端)より大きい（右側）
+      obj1.y < obj2.y + obj2.height &&//ballのy座標がpaddleのy座標+幅より小さい（上側）
+      obj2.y < obj1.y + obj1.height   //ballの高さ+y座標がpaddleのy座標より大きい（下側）
+    );//返却するのはtrueかfalse。trueの場合、ballのxy座標がpaddleのxy座標内部に入った時
   };
 
   ////////////////////////
   //メインの繰り返し処理//
   ////////////////////////
-  const loop = () => {
+  function loop() {
     ////////////////////////////////
     //指定範囲の描画のリセット処理//
     ////////////////////////////////
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     paddle.update(); //パドルの再描画
-    ball.update(); //ボールの再描画
-    block.update(); //ブロックの再描画
+    ball.update();   //ボールの再描画
+    block.update();  //ブロックの再描画
 
     //////////////////////////////////////////////////////
     //ボールとブロックが接触したら、ボールを反射する挙動//
@@ -178,11 +181,11 @@
     //接触したブロックを除き、再配列する処理//
     //////////////////////////////////////////
     block.data.forEach((brick, index) => {
-      if (collide(ball, brick)) {
+      if (collide (ball, brick) ) {
         ball.dy *= -1;
         block.data.splice(index, 1);
       }
-      if(block.data.length === 0){
+      if( block.data.length === 0 ){
         alert("ゲームクリアです！");
         confirm("ゲームを再開しますか？");
       }
@@ -191,7 +194,7 @@
     ////////////////////////////////////////////////
     //ブラウザの適切なタイミングで再描画処理を実行//
     ////////////////////////////////////////////////
-    window.requestAnimationFrame(loop); 
+    requestId = window.requestAnimationFrame(loop); 
     console.log(ball.dx , ball.dy);
   };
 
@@ -228,7 +231,9 @@
     paddle.speed = 0;
   });
 
-  
+  //////////////
+  //難易度設定//
+  //////////////
   function startFlag() {
     levelRow = parseInt(
       window.prompt("難易度を1〜7で選択してください", "7")
@@ -256,15 +261,15 @@
   //ゲーム終了の関数//
   ////////////////////
   function gameOver() {
+    isPlaying = false;
     window.alert("GameOver");
     if(confirm("ゲームを再開しますか")) {
-      isPlaying = true;
+      requestId = 0;
       startFlag();
       init(levelRow); //パドル位置の初期化関数を実行
-      loop(); //画像を再描画する関数の実行
+      isPlaying = true;
     } else {
-      alert("お疲れ様でした！画面を閉じてください。")
-      isPlaying = false;
+      alert("お疲れ様でした！画面を閉じてください。");
     }
   };
 
@@ -272,6 +277,7 @@
   //ゲームクリアの関数//
   //////////////////////
   function gameClear(){
+    window.cancelAnimationFrame(requestId);
     init();
   }
 }
