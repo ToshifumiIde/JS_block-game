19f7a77d51baac5672be195fb5ae0336c0e670d1
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-01 22:31:47 +0900

    ゲーム難易度の選択方法を変更（buttonにて実装）

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 "use strict";
 
-{
+(() => {
   ////////////////////////
   //ゲームスタートの管理//
   ////////////////////////
   let isPlaying = false;//ゲーム実行状況、初期値はfalse
-  let levelRow;         //ゲームレベル、最初は空（後でプレーヤーに入力してもらう）
+  let levelRow;         //ゲームレベル、最初は空（プレーヤーが設定）
   let requestId;        //後述する再描画処理を格納するId
 
+  ///////
+  //DOM//
+  ///////
+  const beginner = document.getElementById("beginner");
+  const intermediate = document.getElementById("intermediate");
+  const advanced = document.getElementById("advanced");
+  const canvas = document.getElementById("canvas");
+  const reset = document.getElementById("reset");
+  
   ////////////////
   //canvasの生成//
   ////////////////
-  const canvas = document.createElement("canvas");//<canvas></canvas>の生成（生成のみ）
+  if(typeof canvas.getContext === "undefined"){
+    return;
+  }
   const ctx = canvas.getContext("2d"); //キャンバス用のメソッドを取得
-  canvas.width = 500;                  //canvasの幅
-  canvas.height = canvas.width * 0.8;  //canvasの高さ
+  // canvas.width = 500;                  //canvasの幅
+  // canvas.height = canvas.width * 0.8;  //canvasの高さ
   canvas.setAttribute (
     "style",
     "display:block; margin:0 auto; background-color:#eee"
-  );                                   //canvasのスタイル
-  document.body.appendChild(canvas);   //canvasをbodyの直下に配置
+  );//canvasのスタイル
 
 
   ////////////////////
@@ -45,11 +55,11 @@
       if (this.y < 0 || this.y > canvas.height) {
         this.dy *= -1;
       }
-      if (this.y > canvas.height) {
-        window.cancelAnimationFrame(requestId);
-        isPlaying = false;
-        gameOver();
-      }
+      // if (this.y > canvas.height) {
+      //   // window.cancelAnimationFrame(requestId);
+      //   isPlaying = false;
+      //   gameOver();
+      // }
       ball.x += this.dx;
       ball.y += this.dy;
     },
@@ -66,8 +76,8 @@
     height: 12,
     speed: 0,
     update: function () {
-      ctx.fillStyle = "green";
       ctx.fillRect(this.x, this.y, this.width, this.height);
+      ctx.fillStyle = "green";
       ctx.fill();
       this.x += this.speed;
     },
@@ -186,23 +196,24 @@
         block.data.splice(index, 1);
       }
       if( block.data.length === 0 ){
+        block.data.splice(index, 1);
         alert("ゲームクリアです！");
         confirm("ゲームを再開しますか？");
       }
     });
-    
     ////////////////////////////////////////////////
     //ブラウザの適切なタイミングで再描画処理を実行//
     ////////////////////////////////////////////////
     requestId = window.requestAnimationFrame(loop); 
-    console.log(ball.dx , ball.dy);
   };
+
 
   ////////////////////////
   //キー操作時のイベント//
   ////////////////////////
   document.addEventListener("keydown", (e) => {
     if(paddle.x <= 0) {
+      paddle.x = 0;
       paddle.speed = 0;
       if (e.key === "ArrowRight") {
         paddle.speed = 6;
@@ -213,6 +224,7 @@
       }
     }
     if (paddle.x + paddle.width >= canvas.width) {
+      paddle.x = canvas.width - paddle.width;
       paddle.speed = 0;
       if (e.key === "ArrowLeft") {
         paddle.speed = -6;
@@ -231,31 +243,17 @@
     paddle.speed = 0;
   });
 
-  //////////////
-  //難易度設定//
-  //////////////
-  function startFlag() {
-    levelRow = parseInt(
-      window.prompt("難易度を1〜7で選択してください", "7")
-      );
-      if (typeof levelRow !== "number") {
-        console.log("数字を入力してください");
-      }
-    }
-    
-
   //////////////////////
   /////スタート処理/////
   //////////////////////
-  document.addEventListener("click", () => {
-    if (isPlaying === true) {
-      return;
-    }
-    startFlag();
-    isPlaying = true;
-    init(levelRow); //パドル位置の初期化関数を実行
-    loop(); //画像を再描画する関数の実行
-  });
+  function gameStart(){      
+      if (isPlaying === true) {
+        return;
+      }
+      isPlaying = true;//ゲームの状態管理
+      init(levelRow);  //パドル位置の初期化関数を実行
+      loop();          //画像を再描画する関数の実行
+  }
 
   ////////////////////
   //ゲーム終了の関数//
@@ -277,7 +275,60 @@
   //ゲームクリアの関数//
   //////////////////////
   function gameClear(){
-    window.cancelAnimationFrame(requestId);
+    // window.cancelAnimationFrame(requestId);
+    alert("ゲームクリアです！");
+    if(window.confirm("ゲームを再開しますか？")){
+    }else{
+    }
     init();
   }
-}
+
+  ////////////////////
+  //ゲーム難易度選択//
+  ////////////////////
+  beginner.addEventListener("click" , () => {
+    if(isPlaying ===true){
+      return;
+    }
+    if(window.confirm("初級レベルでゲームを開始しますか？")){
+      levelRow = 2;
+      gameStart();
+    } else {
+      return;
+    }
+  });
+  intermediate.addEventListener("click" , () => {
+    if(isPlaying === true){
+      return;
+    }
+    if(window.confirm("中級レベルでゲームを開始しますか？")){
+      levelRow = 5;
+      gameStart();
+    } else {
+      return;
+    }
+  });
+  advanced.addEventListener("click" , () => {
+    if(isPlaying === true){
+      return;
+    }
+    if(window.confirm("上級レベルでゲームを開始しますか？")){
+      levelRow = 8;
+      gameStart();
+    } else {
+      return;
+    }
+  });
+
+  reset.addEventListener("click" , () => {
+    if(isPlaying === false){
+      return;
+    }
+    if(window.confirm("ゲームを中止しますか？")){
+      isPlaying = false;
+      cancelAnimationFrame(requestId);
+    } else {
+      console.log("ゲームを再開します");
+    }
+  });
+})();
